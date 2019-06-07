9e15dbd7f0bd1b2666f1af2654920f8e6a8a9cd3
Luiz <argentinaluiz@gmail.com>
2019-06-07 12:54:33 -0300

    First commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /** @var Client $client */
+        $client = app(Client::class);
+        $index = ['index' => env('ES_INDEX')];
+        if (!$client->indices()->exists($index)) {
+            $client->indices()->create($index);
+        }
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Client::class, function () {
+            return ClientBuilder::create()->build();
+        });
+    }
+}
